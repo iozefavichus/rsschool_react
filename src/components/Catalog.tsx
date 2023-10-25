f75fc298c6abdb918b7ba415ab5c7793f14d4e8a
iozefavichus <iozefavichus@gmail.com>
2023-10-25 20:41:00 +0300

    feat: add header and catalog components

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class Catalog extends Component {
+  render() {
+    return <h1>Here will be catalog</h1>;
+  }
+}
+
+export default Catalog;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return <h1>Here will be header</h1>;
+  }
+}
+
+export default Header;
